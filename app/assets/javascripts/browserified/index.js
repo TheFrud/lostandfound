0a9bcda1608e24aab3378aff780f3ed5b3e3a0ca
TheFrud <frud@live.com>
2016-05-11 00:08:34 +0200

    Fixed image src (3)

--- app/assets/javascripts/browserified/index.js
+++ app/assets/javascripts/browserified/index.js
@@ -304,14 +304,10 @@ const addAnnonserToMap = function() {
                 
                 var environmentPath = document.getElementById("environmentPath");
 
-                console.log(window.location.href )
-                console.log(environmentPath.innerHTML)
-
-
                 var contentString = "<div class='map-info-window'>" +
                 "<h3><a href='"+window.location.href+item.id+"'>" + item.rubrik + "</a></h3>" + 
                 "<div class='imgLiquidFill imgLiquid' style='width:50px; height:50px;'>" +
-                "<img class='media-object' src='"+environmentPath.innerHTML+"/assets/images/annons_imgs/"+item.img+"' alt='...'>" +
+                "<img class='media-object' src='"+window.location.href+"/assets/images/annons_imgs/"+item.img+"' alt='...'>" +
                  "</div>" +
                  "<br>" + item.text +
                  "</div>";
